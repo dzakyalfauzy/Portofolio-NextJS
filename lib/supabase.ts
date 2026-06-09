68fbabfe90b6f9bb9668fd4eccf634f2ee058351
Dzaky Alfauzy <dzakyalfauzyy@gmail.com>
2026-06-09 23:49:55 +0700

    fix: restore env vars for supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://dfuprxqhyvpelekncuzv.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmdXByeHFoeXZwZWxla25jdXp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5Mzc3MDEsImV4cCI6MjA5NjUxMzcwMX0.efXSNm5ljZcFrsK_dELNILJfXuke1uKSVRTtR3OUEtQ";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
