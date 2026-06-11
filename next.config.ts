d1cd9b8ef405034b0cfd976ed1e866c808fccc75
Dzaky Alfauzy <dzakyalfauzyy@gmail.com>
2026-06-12 00:03:02 +0700

    fix: add webpack config for GLB files and fix duplicate imports

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,18 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   turbopack: undefined,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "static/media/[name].[hash:8].[ext]",
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
